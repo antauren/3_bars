e08cce793dbe1b88234caed19e8097d827e315ec
Anatoly <antauren@yandex.ru>
2017-03-30 17:26:59 +0300

    устранены следующие замечания: -Используй snake_case для именования переменных (переименуй, например, maxseatscount, min_s, minseatscount) -Некоторые переменные плохо названы, надо переименовать. Сначала почитай про названия переменных. (data) -Вместо табов для отступов стоит использовать пробелы. Избавься от табов и настрой редактор.

--- bars.py
+++ bars.py
@@ -2,55 +2,49 @@ import json
 import sys
 
 def load_data(filepath):
-	with open ( filepath[1] , 'r', encoding='windows-1251' ) as f:
-		data = json.load(f)
-		#print(data[0])
-	return data
-
+    with open ( filepath[1] , 'r', encoding='windows-1251' ) as f:
+        data = json.load(f)
+    return data
 
 def get_biggest_bar(data):
-	maxSeatsCount, index_biggest_bar = data[0]['SeatsCount'], 0
-	for i in range(len(data)):
-		if data[i]['SeatsCount'] > maxSeatsCount:
-			maxSeatsCount = data[i]['SeatsCount']
-			index_biggest_bar = i
-	return data[index_biggest_bar]['Name']
+    max_seats_count, biggest_bar = data[0]['SeatsCount'], data[0]
+    for bar in data:
+        if bar['SeatsCount'] > max_seats_count:
+            max_seats_count = bar['SeatsCount']
+            biggest_bar = bar
+    return biggest_bar['Name']
 
 
 def get_smallest_bar(data):
-	minSeatsCount, index_smallest_bar = data[0]['SeatsCount'], 0
-
-	for i in range(len(data)):
-		if  data[i]['SeatsCount'] < minSeatsCount:
-			minSeatsCount = data[i]['SeatsCount']
-			index_smallest_bar = i
-
-	return data[index_smallest_bar]['Name']
-
-
+    min_seats_count, smallest_bar = data[0]['SeatsCount'], data[0]
+    for bar in data:
+        if  bar['SeatsCount'] < min_seats_count:
+            min_seats_count = bar['SeatsCount']
+            smallest_bar = bar
+    return smallest_bar['Name']
 
 def get_closest_bar(data, longitude, latitude):
-	x1, y1 = longitude, latitude
-	x2, y2 = data[0]['geoData']['coordinates'][0], data[0]['geoData']['coordinates'][1]
-	min_S = ( (x1-x2)**2 + (y1-y2)**2 )**0.5
-	for i in range(len(data)):
-		x2, y2 = data[i]['geoData']['coordinates'][0], data[i]['geoData']['coordinates'][1]
-		if ( (x1-x2)**2 + (y1-y2)**2 )**0.5 < min_S:
-			min_S = ( (x1-x2)**2 + (y1-y2)**2 )
-			index_closest_bar = i
-	return data[index_closest_bar]['Name']
+    closest_bar = data[0]
+    x1, y1 = longitude, latitude
+    x2, y2 = data[0]['geoData']['coordinates'][0], data[0]['geoData']['coordinates'][1]
+    min_distance = ( (x1-x2)**2 + (y1-y2)**2 )**0.5
+
+    for bar in data:
+        x2, y2 = bar['geoData']['coordinates'][0], bar['geoData']['coordinates'][1]
+        if ( (x1-x2)**2 + (y1-y2)**2 )**0.5 < min_distance:
+            min_distance = ( (x1-x2)**2 + (y1-y2)**2 )
+            closest_bar = bar
+    return closest_bar['Name']
 
 
 def get_coordinates(string):
-	try:
-		x1, y1 = string.split()
-		x1, y1 = float(x1), float(y1)
-		return [x1, y1]
-	except ValueError:
-		print('Введите ДВА ЧИСЛА через ПРОБЕЛ: ')
-		return get_coordinates( input() )
-
-
+    try:
+        x1, y1 = string.split()
+        x1, y1 = float(x1), float(y1)
+        return [x1, y1]
+    except ValueError:
+        print('Введите ДВА ЧИСЛА через ПРОБЕЛ: ')
+        return get_coordinates( input() )
 
 filepath = sys.argv
 data = load_data(filepath)
