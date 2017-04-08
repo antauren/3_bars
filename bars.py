bbaf627a9de032c0905554e8c17eaf169068e921
antauren <antauren@yandex.ru>
2017-04-08 08:21:10 +0300

    исправлены следующие замечания:
    -Некоторые переменные плохо названы, надо переименовать. Сначала почитай про названия переменных. (x1, x2, temp, data – это всё примеры плохо названных переменных; подробнее глянь в энциклопедии)
    
    -Используй встроенные функции Python. Документация. (почитай про min/max и используй их для поиска баров)

--- bars.py
+++ bars.py
@@ -5,58 +5,38 @@ def load_data(filepath):
     with open ( filepath[1] , 'r', encoding='windows-1251' ) as f:
         return json.load(f)
 
-def get_biggest_bar(data):
-    max_seats_count, biggest_bar = data[0]['SeatsCount'], data[0]
-    for bar in data:
-        if bar['SeatsCount'] > max_seats_count:
-            max_seats_count = bar['SeatsCount']
-            biggest_bar = bar
+def get_biggest_bar(bars_data):
+    biggest_bar = max((bar for bar in bars_data), key=lambda bar: bar['SeatsCount'])
     return biggest_bar['Name']
 
 
-def get_smallest_bar(data):
-    min_seats_count, smallest_bar = data[0]['SeatsCount'], data[0]
-    for bar in data:
-        if  bar['SeatsCount'] < min_seats_count:
-            min_seats_count = bar['SeatsCount']
-            smallest_bar = bar
+def get_smallest_bar(bars_data):
+    smallest_bar = min((bar for bar in bars_data), key=lambda bar: bar['SeatsCount'])
     return smallest_bar['Name']
 
-def get_closest_bar(data, longitude, latitude):
-    closest_bar = data[0]
-    x1, y1 = longitude, latitude
-    x2, y2 = data[0]['geoData']['coordinates'][0], data[0]['geoData']['coordinates'][1]
-    min_distance = ( (x1-x2)**2 + (y1-y2)**2 )**0.5
-
-    for bar in data:
-        x2, y2 = bar['geoData']['coordinates'][0], bar['geoData']['coordinates'][1]
-        if ( (x1-x2)**2 + (y1-y2)**2 )**0.5 < min_distance:
-            min_distance = ( (x1-x2)**2 + (y1-y2)**2 )
-            closest_bar = bar
+def get_closest_bar(bars_data, longitude, latitude):
+    closest_bar = min((bar for bar in bars_data), key = lambda bar:((longitude - bar['geoData']['coordinates'][0])**2 + (latitude - bar['geoData']['coordinates'][1])**2 )**0.5)
     return closest_bar['Name']
-
 
 def get_coordinates(string):
     try:
-        x1, y1 = string.split()
-        x1, y1 = float(x1), float(y1)
-        return [x1, y1]
+        longitude, latitude = string.split()
+        return [float(longitude), float(latitude)]
     except ValueError:
-        print('Введите ДВА ЧИСЛА через ПРОБЕЛ: ')
+        print('Введите ДВА ЧИСЛА(широта и долгота) через ПРОБЕЛ: ')
         return get_coordinates( input() )
 
 filepath = sys.argv
-data = load_data(filepath)
+bars_data = load_data(filepath)
 
-print('самый большой бар: ', get_biggest_bar(data))
-print('самый маленький бар:', get_smallest_bar(data))
+print('самый большой бар: ', get_biggest_bar(bars_data))
+print('самый маленький бар:', get_smallest_bar(bars_data))
 print('для того чтобы узнать ближайший к Вам бар, введите координаты (два числа через пробел): ')
 
-temp = get_coordinates(input())
-longitude, latitude = temp[0], temp[1]
+coordinates = get_coordinates(input())
+longitude, latitude = coordinates[0], coordinates[1]
 
-print('самый близкий бар:', get_closest_bar(data, longitude, latitude))
-
+print('самый близкий бар:', get_closest_bar(bars_data, longitude, latitude))
 
 if __name__ == '__main__':
     pass
