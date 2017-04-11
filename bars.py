f8d020166216469435cf300e5f18e7de39fa68ca
antauren <antauren@yandex.ru>
2017-04-11 11:37:24 +0300

    исправлено: -Давай избавимся от большинства try...except, они увеличивают вложенность и усложняют код. Например, вместо оборачивания кода в except FileNotFoundError можно вначале проверить, существует ли файл.

--- bars.py
+++ bars.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import os.path
 
 def load_data(filepath):
     with open ( filepath , 'r', encoding='windows-1251' ) as f:
@@ -26,53 +27,41 @@ def get_coordinates(string):
 
         return get_coordinates( input() )
 
-def try_load_data(filepath):
-    try:
-        bars_data = load_data(filepath)
-        print('самый большой бар: ', get_biggest_bar(bars_data))
-        print('самый маленький бар:', get_smallest_bar(bars_data))
-        print('для того чтобы узнать ближайший к Вам бар, введите gps-координаты (широта и долгота) через пробел: ')
 
-        coordinates = get_coordinates(input())
-        longitude, latitude = coordinates[0], coordinates[1]
-
-        print('самый близкий бар:', get_closest_bar(bars_data, longitude, latitude))
-
-    except FileNotFoundError:
-        bars_data = None
-
-    if bars_data is None:
-        print('Ошибка: программа не может найти файл с базой данных баров')
-        print('Воспользуйтесь помощью (--help)')
-
-def try_get_filepath(argv_list):
-    try:
-        filepath = argv_list[1]
-
-        if filepath == '--help':
-            print('\nЧтобы запустить программу, нужно ввести в консоли: <интерпретатор> <скрипт> <база_данных_json>')
-            print('Наример: python3 bars.py data-2897-2016-11-23.json\n')
-            print('Актуальную базу данных можно скачать с сайта открытых данных https://data.mos.ru/opendata/7710881420-bary')
-            print('Файл базы данных нужно положить в папку, где находится скрипт bar.py\n')
-
-        else:
-            try_load_data(filepath)
-            
-
-    except IndexError:
-        filepath = None
-
-    if filepath is None:
+def start_script(argv_list):
+    if len(argv_list) < 2:
         print('Вас привествует программа поиска баров Москвы')
         print('Скрипт рассчитывает:')
         print('\t-самый большой бар')
         print('\t-самый маленький бар')
         print('\t-ближайший бар')
         print('Для того чтоб узнать как работает программа введите: python bars.py --help или python3 bars.py --help')
-    
-    return filepath
+        return None
+
+    filepath = argv_list[1]
+
+    if filepath == '--help':
+        print('\nЧтобы запустить программу, нужно ввести в консоли: <интерпретатор> <скрипт> <база_данных_json>')
+        print('Наример: python3 bars.py data-2897-2016-11-23.json\n')
+        print('Актуальную базу данных можно скачать с сайта открытых данных https://data.mos.ru/opendata/7710881420-bary')
+        print('Файл базы данных нужно положить в папку, где находится скрипт bar.py\n')
+        return None
+
+    if not os.path.exists(filepath):
+        print('Ошибка: программа не может найти файл с базой данных баров')
+        print('Воспользуйтесь помощью (--help)')
+        return None
+
+    bars_data = load_data(filepath)
+    print('самый большой бар: ', get_biggest_bar(bars_data))
+    print('самый маленький бар:', get_smallest_bar(bars_data))
+    print('для того чтобы узнать ближайший к Вам бар, введите gps-координаты (широта и долгота) через пробел: ')
+
+    coordinates = get_coordinates(input())
+    longitude, latitude = coordinates[0], coordinates[1]
+    print('самый близкий бар:', get_closest_bar(bars_data, longitude, latitude))
 
 
 if __name__ == '__main__':
     argv_list = sys.argv
-    try_get_filepath(argv_list)
+    start_script(argv_list)
